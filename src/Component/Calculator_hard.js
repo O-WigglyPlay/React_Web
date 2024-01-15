67ecd62d4ed07c25f6929c9e9ebf90b30250a590
오재준 <dhehfud00@naver.com>
2024-01-15 22:25:34 +0900

    계산기 완성

--- src/Component/Calculator_hard.js
+++ src/Component/Calculator_hard.js
@@ -1,17 +1,17 @@
-import react, {useState} from 'react';
+import react, { useState } from 'react';
 import styles from './Calculator.module.css'
 
 const Calculator_hard = () => {
   const [Value, setValue] = useState(0);
   const [OldValue, setOldValue] = useState(0);
   const [operator, setOperator] = useState(0);
-  
+
   const inputValue = (e) => {
-    if (Value === 0){
-      setValue(e.target.value);
-      console.log(e.target.value);
-    } else{
-      setValue(e.target.Value + Value);
+    let input = e.target.value;
+    if (Value === 0) {
+      setValue(input);
+    } else {
+      setValue(Value + input);
     }
   }
 
@@ -24,45 +24,59 @@ const Calculator_hard = () => {
   }
 
   const changeSign = () => {
-    if(Value > 0){
+    if (Value > 0) {
       setValue(-Value);
     } else {
       setValue(Math.abs(Value));
     }
   }
-  
-  const operatorHandler = (e) => {
-    let operlatorInput = e.target.value;
-    setOperator(operlatorInput);
+
+  const operatorHandle = (e) => {
+    let operatorInput = e.target.value;
+    setOperator(operatorInput);
     setOldValue(Value);
     setValue(0);
   }
 
   const calculate = () => {
-    if(operator === "/"){
+    if (operator === "/") {
       setValue(parseFloat(OldValue) / parseFloat(Value));
-    } else if(operator === "X"){
+    } else if (operator === "X") {
       setValue(parseFloat(OldValue) * parseFloat(Value));
-    } else if(operator === "-"){
+    } else if (operator === "-") {
       setValue(parseFloat(OldValue) - parseFloat(Value));
-    } else if(operator === "+"){
+    } else if (operator === "+") {
       setValue(parseFloat(OldValue) + parseFloat(Value));
     }
   }
 
-  return(
+  return (
     <div className="CalculatorArea">
       <div className={styles.container}>
         <div className={styles.wrapper}>
           <div className="Display">
             <h1 className={styles.result}>{Value}</h1>
-            <button onClick={clear}>AC</button>
-            <button onClick={changeSign}>+/-</button>
-            <button onClick={percent}>%</button>
-            <button onClick={operatorHandler}>+</button>
-            <button onClick={inputValue} value={1}>1</button>
-            <button onClick={inputValue} value={2}>2</button>
-            <button onClick={calculate}>=</button>
+          </div>
+          <div className={styles.btn_container}>
+            <button className={styles.gary} onClick={clear}>AC</button>
+            <button className={styles.gary} onClick={changeSign}>+/-</button>
+            <button className={styles.gary} onClick={percent}>%</button>
+            <button className={styles.orange} onClick={operatorHandle} value={'/'}>÷</button>
+            <button className={styles.deepgray} onClick={inputValue} value={7}>7</button>
+            <button className={styles.deepgray} onClick={inputValue} value={8}>8</button>
+            <button className={styles.deepgray} onClick={inputValue} value={9}>9</button>
+            <button className={styles.orange} onClick={operatorHandle} value={'X'}>X</button>
+            <button className={styles.deepgray} onClick={inputValue} value={4}>4</button>
+            <button className={styles.deepgray} onClick={inputValue} value={5}>5</button>
+            <button className={styles.deepgray} onClick={inputValue} value={6}>6</button>
+            <button className={styles.orange} onClick={operatorHandle} value={'-'}>-</button>
+            <button className={styles.deepgray} onClick={inputValue} value={1}>1</button>
+            <button className={styles.deepgray} onClick={inputValue} value={2}>2</button>
+            <button className={styles.deepgray} onClick={inputValue} value={3}>3</button>
+            <button className={styles.orange} onClick={operatorHandle} value={'+'}>+</button>
+            <button className={styles.zero} onClick={inputValue} value={0}>0</button>
+            <button className={styles.deepgray} onClick={inputValue} value={"."}>.</button>
+            <button className={styles.orange} onClick={calculate}>=</button>
           </div>
         </div>
       </div>
