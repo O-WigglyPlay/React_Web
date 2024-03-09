8dda70d29863e047da4485e52eb9983e1750041a
오재준 <dhehfud00@naver.com>
2024-03-09 22:00:22 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
@@ -79,14 +80,14 @@ const InputForm = styled.form`
     height: 20px;
     color: rgba(0, 0, 0, 0.8);
     position: absolute;
-    top: 35%;
+    top: 10px;
     left: 6px;
   }
 `;
 
 const TodoInput = styled.input`
   width: 100%;
-  height: 7vh;
+  height: 40px;
   border-radius: 8px;
   border: solid 1px rgba(0, 0, 0, 0.3);
   padding-left: 30px;
@@ -110,49 +111,100 @@ const TodoList = styled.div`
   justify-content: space-between;
   margin: 12px 0;
 
-  svg{
+  svg {
     width: 30px;
   }
 `;
 
 const TodoFirst = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-input{
-  margin-right: 20px;
-}
-h2{
-  margin-top: 4px;
-}
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  input {
+    margin-right: 20px;
+  }
+  h2 {
+    margin-top: 4px;
+  }
+`;
 
 const TodoBottom = styled.div`
-height: 10vh;
-display: flex;
-align-items: center;
-justify-content: space-between;
-`
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const TodoSuccess = styled.div`
   font-weight: bolder;
-`
+`;
 
 const TodoBtns = styled.div`
-button:first-child{
-  margin-right: 10px;
-}
-`
+  button:first-child {
+    margin-right: 10px;
+  }
+`;
 
 const TodoBtn = styled.button`
-border: 0;
-border-radius: 4px;
-background-color: rgb(108,117,125);
-padding: 7px;
-color: white;
-`
+  border: 0;
+  border-radius: 4px;
+  background-color: rgb(108, 117, 125);
+  padding: 7px;
+  color: white;
+`;
 
 function App() {
+  const [group, setGroup] = useState([
+    { id: 0, check: false, content: "Exercise" },
+    { id: 1, check: false, content: "Study" },
+    { id: 2, check: false, content: "Shopping" },
+  ]);
+
+  const onCheckHandle = (e) => {
+    const todoId = +e.target.value;
+    setGroup((prev) => {
+      const targetTodoIndex = prev.findIndex((todoObj) => {
+        return todoObj.id === todoId;
+      });
+      const newTodo = {
+        content: prev[targetTodoIndex].content,
+        check: !prev[targetTodoIndex].check,
+        id: todoId,
+      };
+      return [
+        ...prev.slice(0, targetTodoIndex),
+        newTodo,
+        ...prev.slice(targetTodoIndex + 1),
+      ];
+    });
+  };
+
+  const onDelHandle = (deleteId) => {
+    const todoId = +deleteId;
+    setGroup((prev) => {
+      const targetTodoIndex = prev.findIndex((value) => value.id === todoId);
+      return [
+        ...prev.slice(0, targetTodoIndex),
+        ...prev.slice(targetTodoIndex + 1),
+      ];
+    });
+  };
+
+  const completeCheck = group.filter((value) => value.check === true).length;
+
+  const allClear = () => {
+    setGroup([]);
+  };
+
+  const onSelDelHandle = () => {
+    setGroup((prev) => prev.filter((value) => !value.check));
+  };
+  
+
+  const inputTodoList = () => {
+
+  }
+
   return (
     <>
       <GlobalStyle />
@@ -176,83 +228,48 @@ function App() {
             />
           </svg>
 
-          <TodoInput type="text" placeholder="Please enter your ToDo List" />
+          <TodoInput type="text" placeholder="Please enter your ToDo List"/>
         </InputForm>
         <TodoContents>
-          <TodoTitle>
-            List
-          </TodoTitle>
+          <TodoTitle>List</TodoTitle>
           <TodoLists>
-            <TodoList>
-              <TodoFirst>
-              <input type="checkbox" name="" id="" />
-              <h2>Exercise</h2>
-              </TodoFirst>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                />
-              </svg>
-            </TodoList>
-            <TodoList>
-              <TodoFirst>
-              <input type="checkbox" name="" id="" />
-              <h2>Study</h2>
-              </TodoFirst>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                />
-              </svg>
-            </TodoList>
-            <TodoList>
-              <TodoFirst>
-              <input type="checkbox" name="" id="" />
-              <h2>Shopping</h2>
-              </TodoFirst>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                />
-              </svg>
-            </TodoList>
+            {group.map((x) => (
+              <TodoList>
+                <TodoFirst>
+                  <input
+                    type="checkbox"
+                    name=""
+                    value={x.id}
+                    checked={x.check}
+                    onChange={onCheckHandle}
+                  />
+                  <h2>{x.content}</h2>
+                </TodoFirst>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                  onClick={()=>onDelHandle(x.id)}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                  />
+                </svg>
+              </TodoList>
+            ))}
           </TodoLists>
           <TodoBottom>
-            <TodoSuccess><h3>Completed Todos : 3</h3></TodoSuccess>
+            <TodoSuccess>
+              <h3>Completed Todos : {completeCheck}</h3>
+            </TodoSuccess>
             <TodoBtns>
-              <TodoBtn>
-                Delete selected
-              </TodoBtn>
-              <TodoBtn>
-                Clear all
-              </TodoBtn>
+              <TodoBtn onClick={onSelDelHandle}>Delete selected</TodoBtn>
+              <TodoBtn onClick={allClear}>Clear all</TodoBtn>
             </TodoBtns>
           </TodoBottom>
         </TodoContents>
